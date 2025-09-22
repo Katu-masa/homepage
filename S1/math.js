ce14339c6512dd7bdaa649973e8ea3065a3fca9d
Katu-masa <5mikoto5@gmail.com>
2025-09-22 12:00:06 +0900

    Update math.js

--- S1/math.js
+++ S1/math.js
@@ -160,11 +160,41 @@ function simplsqrt(sqrt) {
     }
     return ans;
 }
+function getRandomIntBetween(min, max) {
+    // minとmaxが逆の場合に対応
+    if (min > max) [min, max] = [max, min];
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function polynomialToString(coefficients) {
+    let terms = [];
+
+    for (let i = coefficients.length - 1; i >= 0; i--) {
+        let coef = coefficients[i];
+        if (coef === 0) continue;
+
+        let term = '';
+        if (i === 0) {
+            term = `${coef}`;
+        } else if (i === 1) {
+            term = coef === 1 ? 'x' : coef === -1 ? '-x' : `${coef}x`;
+        } else {
+            term = coef === 1 ? `x^${i}` : coef === -1 ? `-x^${i}` : `${coef}x^${i}`;
+        }
+
+        terms.push(term);
+    }
+
+    return terms.length > 0 ? terms.join(' + ').replace(/\+ -/g, '- ') : '0';
+}
+
+
 function printfrac(fracc) {
     let frac = [];
     for (let i = 0; i < 4; i++) {
         frac[i] = fracc[i];
     }
+    frac = simplfrac(frac);
     let pfrac = [];
     let pfrag = '';
     if (frac[0] < 0) {
@@ -182,34 +212,34 @@ function printfrac(fracc) {
             pfrac[0] = `1`;
         }
         else {
-            pfrac[0] = `\\sqrt{${frac[1]}}`;
+            pfrac[0] = `\\sqrt{${frac[1]} } `;
         }
     }
     else if (frac[1] == 1) {
-        pfrac[0] = `${frac[0]}`;
+        pfrac[0] = `${frac[0]} `;
     }
     else {
-        pfrac[0] = `${frac[0]}\\sqrt\{ ${frac[1]} \}`
+        pfrac[0] = `${frac[0]} \\sqrt\{ ${frac[1]} \ } `
     }
     if (frac[2] == 1) {
         if (frac[3] == 1) {
             pfrac[1] = `1`;
         }
         else {
-            pfrac[1] = `\\sqrt\{ ${frac[3]}\} `;
+            pfrac[1] = `\\sqrt\{ ${frac[3]} \ } `;
         }
     }
     else if (frac[3] == 1) {
-        pfrac[1] = `${frac[2]}`;
+        pfrac[1] = `${frac[2]} `;
     }
     else {
-        pfrac[1] = `${frac[2]}\\sqrt\{ ${frac[3]} \}`
+        pfrac[1] = `${frac[2]} \\sqrt\{ ${frac[3]} \ } `
     }
 
     if (pfrac[1] == 1 || frac[0] == 0) {
-        return `${pfrac[0]}`;
+        return `${pfrac[0]} `;
     }
     else {
-        return `${pfrag}\\frac\{ ${pfrac[0]} \}\{ ${pfrac[1]} \}`;
+        return `${pfrag} \\frac\{ ${pfrac[0]} \ } \{ ${pfrac[1]} \ } `;
     }
 }
